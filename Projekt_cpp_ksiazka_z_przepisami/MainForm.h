9e2326c677170f6116a57b15687aca3705ad8294
emik0411 <emik0411@LAPTOP-7169H7VB>
2024-11-01 21:53:20 +0100

    Dodaj pliki projektów.

---
+++ Projekt_cpp_ksiazka_z_przepisami/MainForm.h
@@ -0,0 +1,114 @@
+﻿#pragma once
+
+namespace Projektcppksiazkazprzepisami {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Podsumowanie informacji o MainForm
+	/// </summary>
+	public ref class MainForm : public System::Windows::Forms::Form
+	{
+	public:
+		MainForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: W tym miejscu dodaj kod konstruktora
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Wyczyść wszystkie używane zasoby.
+		/// </summary>
+		~MainForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::Button^ button3;
+	protected:
+
+	private:
+		/// <summary>
+		/// Wymagana zmienna projektanta.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+		/// jej zawartości w edytorze kodu.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(21, 98);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(100, 38);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"Dodawanie";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MainForm::button1_Click);
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(143, 101);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(103, 35);
+			this->button2->TabIndex = 1;
+			this->button2->Text = L"Oglądanie";
+			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &MainForm::button2_Click);
+			// 
+			// button3
+			// 
+			this->button3->Location = System::Drawing::Point(73, 173);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(115, 41);
+			this->button3->TabIndex = 2;
+			this->button3->Text = L"Zdjęcie";
+			this->button3->UseVisualStyleBackColor = true;
+			this->button3->Click += gcnew System::EventHandler(this, &MainForm::button3_Click);
+			// 
+			// MainForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(284, 261);
+			this->Controls->Add(this->button3);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->button1);
+			this->Name = L"MainForm";
+			this->Text = L"MainForm";
+			this->Load += gcnew System::EventHandler(this, &MainForm::MainForm_Load);
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
+	private: System::Void button3_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
+	private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
+	private: System::Void MainForm_Load(System::Object^ sender, System::EventArgs^ e) {
+	}
+	};
+}

c58d03833d96f5ebd271377d073c1003d438afe0
Emik0411 <emilka.biesiada@o2.pl>
2024-11-04 15:18:56 +0100

    Zmiany - 04.11.2024

--- Projekt_cpp_ksiazka_z_przepisami/MainForm.h
+++ Projekt_cpp_ksiazka_z_przepisami/MainForm.h
@@ -1,4 +1,6 @@
 ﻿#pragma once
+#include "DodawanieForm.h"
+
 
 namespace Projektcppksiazkazprzepisami {
 
@@ -35,8 +37,13 @@ namespace Projektcppksiazkazprzepisami {
 			}
 		}
 	private: System::Windows::Forms::Button^ button1;
-	private: System::Windows::Forms::Button^ button2;
+
+	private: System::Windows::Forms::ListBox^ listBox1;
 	private: System::Windows::Forms::Button^ button3;
+	private: System::Windows::Forms::Button^ button2;
+
+
+
 	protected:
 
 	private:
@@ -53,13 +60,15 @@ namespace Projektcppksiazkazprzepisami {
 		void InitializeComponent(void)
 		{
 			this->button1 = (gcnew System::Windows::Forms::Button());
-			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->listBox1 = (gcnew System::Windows::Forms::ListBox());
 			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(21, 98);
+			this->button1->Location = System::Drawing::Point(12, 11);
+			this->button1->Margin = System::Windows::Forms::Padding(3, 2, 3, 2);
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(100, 38);
 			this->button1->TabIndex = 0;
@@ -67,34 +76,47 @@ namespace Projektcppksiazkazprzepisami {
 			this->button1->UseVisualStyleBackColor = true;
 			this->button1->Click += gcnew System::EventHandler(this, &MainForm::button1_Click);
 			// 
+			// listBox1
+			// 
+			this->listBox1->FormattingEnabled = true;
+			this->listBox1->ItemHeight = 16;
+			this->listBox1->Location = System::Drawing::Point(12, 98);
+			this->listBox1->Name = L"listBox1";
+			this->listBox1->Size = System::Drawing::Size(261, 276);
+			this->listBox1->TabIndex = 2;
+			this->listBox1->SelectedIndexChanged += gcnew System::EventHandler(this, &MainForm::listBox1_SelectedIndexChanged);
+			// 
+			// button3
+			// 
+			this->button3->Location = System::Drawing::Point(334, 120);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(140, 48);
+			this->button3->TabIndex = 3;
+			this->button3->Text = L"Szukaj";
+			this->button3->UseVisualStyleBackColor = true;
+			this->button3->Click += gcnew System::EventHandler(this, &MainForm::button3_Click_1);
+			// 
 			// button2
 			// 
-			this->button2->Location = System::Drawing::Point(143, 101);
+			this->button2->Location = System::Drawing::Point(138, 11);
+			this->button2->Margin = System::Windows::Forms::Padding(3, 2, 3, 2);
 			this->button2->Name = L"button2";
-			this->button2->Size = System::Drawing::Size(103, 35);
+			this->button2->Size = System::Drawing::Size(100, 38);
 			this->button2->TabIndex = 1;
 			this->button2->Text = L"Oglądanie";
 			this->button2->UseVisualStyleBackColor = true;
 			this->button2->Click += gcnew System::EventHandler(this, &MainForm::button2_Click);
 			// 
-			// button3
-			// 
-			this->button3->Location = System::Drawing::Point(73, 173);
-			this->button3->Name = L"button3";
-			this->button3->Size = System::Drawing::Size(115, 41);
-			this->button3->TabIndex = 2;
-			this->button3->Text = L"Zdjęcie";
-			this->button3->UseVisualStyleBackColor = true;
-			this->button3->Click += gcnew System::EventHandler(this, &MainForm::button3_Click);
-			// 
 			// MainForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(284, 261);
+			this->ClientSize = System::Drawing::Size(544, 429);
 			this->Controls->Add(this->button3);
+			this->Controls->Add(this->listBox1);
 			this->Controls->Add(this->button2);
 			this->Controls->Add(this->button1);
+			this->Margin = System::Windows::Forms::Padding(3, 2, 3, 2);
 			this->Name = L"MainForm";
 			this->Text = L"MainForm";
 			this->Load += gcnew System::EventHandler(this, &MainForm::MainForm_Load);
@@ -103,12 +125,22 @@ namespace Projektcppksiazkazprzepisami {
 		}
 #pragma endregion
 	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		this->Hide();
+		DodawanieForm^ obj1 = gcnew DodawanieForm(this);
+		obj1->ShowDialog();
 	}
 	private: System::Void button3_Click(System::Object^ sender, System::EventArgs^ e) {
 	}
 	private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
+		listBox1->Items->Clear();
 	}
 	private: System::Void MainForm_Load(System::Object^ sender, System::EventArgs^ e) {
 	}
-	};
+
+private: System::Void button3_Click_1(System::Object^ sender, System::EventArgs^ e) {
+	listBox1->Items->Add("Emilka");
+}
+private: System::Void listBox1_SelectedIndexChanged(System::Object^ sender, System::EventArgs^ e) {
+}
+};
 }
